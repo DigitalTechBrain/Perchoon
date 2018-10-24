3b2e973cf8be14a5053a0c792baf16144992ecdb
Jitender <user@USER-PC>
2018-10-24 16:06:34 +0530

    dd

--- Perchoon/Perchoon/App.xaml.cs
+++ Perchoon/Perchoon/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Perchoon
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Perchoon.Login();
         }
 
         protected override void OnStart()
---
+++ Perchoon/Perchoon/Login.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Perchoon
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Login_Button(object sender,EventArgs e)
+        {
+            //DisplayAlert("Login", "Login Sucessfull", "Ok");
+
+            await Navigation.PushAsync(new MainPage());
+        }
+
+    }
+}
